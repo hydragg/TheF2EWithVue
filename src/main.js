9bf08a094abcca9eb2fbcf31048fa4352e2998e1
Hydra <hydragg@gmail.com>
2018-06-18 21:39:55 +0800

    use font-awesome5

--- src/main.js
+++ src/main.js
@@ -2,8 +2,29 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// for IE
+import "babel-polyfill";
+// plugin
+// iView
+import iView from "iview";
+import locale from "iview/dist/locale/zh-TW";
+import "iview/dist/styles/iview.css";
+// font-awesome
+import fontawesome from "@fortawesome/fontawesome";
+import FontAwesomeIcon from "@fortawesome/vue-fontawesome";
+import solid from "@fortawesome/fontawesome-free-solid";
+import regular from "@fortawesome/fontawesome-free-regular";
+import brands from "@fortawesome/fontawesome-free-brands";
 
 Vue.config.productionTip = false;
+
+Vue.use(iView, { locale });
+
+fontawesome.library.add(solid);
+fontawesome.library.add(regular);
+fontawesome.library.add(brands);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,

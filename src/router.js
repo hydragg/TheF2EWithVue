2feac86ea81e3a4e3adc0fc5b28600ec3fb91fdf
Hydra <hydragg@gmail.com>
2018-06-24 20:52:10 +0800

    all page path

--- src/router.js
+++ src/router.js
@@ -16,6 +16,59 @@ export const router = new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/is-sign-up",
+      name: "is-sign-up"
+    },
+    {
+      path: "/search",
+      name: "search"
+    },
+    {
+      path: "/stage-check",
+      name: "stage-check"
+    },
+    {
+      path: "thef2e",
+      name: "thef2e",
+      children: [
+        {
+          path: "week1",
+          name: "week1",
+          meta: { title: "TodoList" }
+        },
+        {
+          path: "week2",
+          name: "week2",
+          meta: { title: "Filter" }
+        },
+        {
+          path: "week3",
+          name: "week3",
+          meta: { title: "Admin Order" }
+        },
+        {
+          path: "week4",
+          name: "week4"
+        },
+        {
+          path: "week5",
+          name: "week5"
+        },
+        {
+          path: "week6",
+          name: "week6"
+        },
+        {
+          path: "week7",
+          name: "week7"
+        },
+        {
+          path: "week8",
+          name: "week8"
+        }
+      ]
     }
   ]
 });
@@ -26,4 +79,3 @@ router.beforeEach((to, from, next) => {
     next();
   }
 });
-
